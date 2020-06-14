3a7047f843f1f89e12b618397d561a45762634a7
Black-Dong <1763424201@qq.com>
2020-06-14 15:52:06 +0800

    Initial commit

---
+++ test/top/codingdong/java/util/HashMapLearn.java
@@ -0,0 +1,20 @@
+package top.codingdong.java.util;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @ClassName HashMapLearn
+ * @Description TODO
+ * @Author Dong
+ * @Date 2020/6/14 15:18
+ */
+public class HashMapLearn {
+
+    @Test
+    public void HashMapPut(){
+        Map<String,Object> map = new HashMap<>();
+    }
+}
